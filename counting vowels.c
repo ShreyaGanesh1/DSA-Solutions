2d0c5352c4e752af63a0e69b882ede29090eba33
ShreyaGanesh1 <sg9523@srmist.edu.in>
2026-01-02 18:49:28 +0530

    Create counting vowels.c

---
+++ counting vowels.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+char s[50]; int count = 0;
+scanf("%s", s);
+for(int i = 0; s[i] != '\0'; i++)
+if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') count++;
+printf("%d", count);
+return 0;
+}
